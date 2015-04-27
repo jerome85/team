cc2e9aeb41b2706c71f5fc4c54894a89fe3cc7f2
Wink Strategies iMac 1 <yan.bbouchard@gmail.com>
2015-04-27 12:48:49 -0400

    ajout des texts

--- index.php
+++ index.php
@@ -16,18 +16,122 @@
   </head>
   <body>
   	<div class="container">
-  		<div class="row">
-  			<div class="col-xs-4 col-sm-3 col-lg-2"><img src="img/equipe1.png"/></div>
-  			<div class="col-xs-4 col-sm-3 col-lg-2"><img src="img/equipe2.png"/></div>
-  			<div class="col-xs-4 col-sm-3 col-lg-2"><img src="img/equipe3.png"/></div>
-  			<div class="col-xs-4 col-sm-3 col-lg-2"><img src="img/equipe4.png"/></div>
-  			<div class="col-xs-4 col-sm-3 col-lg-2"><img src="img/equipe5.png"/></div>
-  			<div class="col-xs-4 col-sm-3 col-lg-2"><img src="img/equipe6.png"/></div>
-  		</div>
+      <div class="row">
+        <div class="col-md-5">
+          <p>Aliston est composée de gens professionnels et compétents qui, 
+          grâce à leur complémentarité, arrivent à identifier, analyser et réaliser 
+          des investissements à haute valeur ajoutée tout en contrôlant le risque.</p>
+          <p>Par la mise en place d’une structure opérationnelle dynamique et innovatrice, 
+            notre équipe assure une gestion active et un suivi efficace de chaque 
+            investissement.</p>
+        </div>
+      </div>
+    </div>
+    <div id="photo">
+      <div class="row">
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe1.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe2.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe3.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe4.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe5.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe6.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe1.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe2.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe3.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe4.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe5.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe6.png"/></div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-3"><h3>Stéphane Bégin, LL.B.</h3></div>
+        <div class="col-md-5"><p>Avocat de formation, Stéphane est diplômé en droit de l’Université 
+          de Montréal et membre du Barreau du Québec. Après avoir exercé le droit en pratique privée, 
+          il s’est rapidement orienté vers le milieu des affaires. Depuis 1995, avec son équipe, il a 
+          procédé à une série d’acquisitions d’entreprises œuvrant dans différents secteurs 
+          d’activités. Fondateur d’Aliston, il y siège au conseil d’administration et agit à 
+          titre de président. Enfin, il siège au conseil d’administration de nombreuses sociétés dans 
+          lesquelles le groupe détient des participations.</p></div>
+        <div class="col-md-2"></div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-3"><h3>Olivier L.-Labonté, LL.B., MBA</h3></div>
+        <div class="col-md-5"><p>Olivier agit à titre de directeur, affaires juridiques. 
+          Avocat de formation et membre du Barreau du Québec, il est également titulaire 
+          d’une maîtrise en administration des affaires (MBA). Avant de se joindre à notre 
+          équipe, Olivier a pratiqué au sein de deux importants cabinets d’avocats canadiens. 
+          Ses connaissances et son expertise dans les domaines des services financiers, 
+          du droit transactionnel et du droit commercial lui permettent d’adopter une approche 
+          pratique et d’offrir des conseils à toutes les étapes des activités 
+          d’investissements.</p></div>
+        <div class="col-md-2"></div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-3"><h3>Caroline Martel, CPA.CA</h3></div>
+        <div class="col-md-5"><p>Caroline agit à titre de vice-présidente, finance et 
+          investissements. Comptable professionnelle agréée (CPA.CA), elle exerce sa 
+          profession depuis treize ans. Ayant d’abord travaillé au sein de grands cabinets, 
+          elle a ensuite occupé le poste de Directrice des finances d’une société publique 
+          canadienne en Europe et en Amérique Centrale.<br/>Forte de ces expériences, 
+          Caroline met aujourd’hui ses compétences au profit de la société lors de l
+          ’évaluation des opportunités d’affaires et du financement, en plus de veiller à 
+          la saine gestion financière de l’entreprise.</p></div>
+        <div class="col-md-2"></div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-3"><h3>Jose Imbaquingo</h3></div>
+        <div class="col-md-5"><p>José agit à titre de contrôleur. Il a acquis son expertise au sein 
+          de grands cabinets comptables en Amérique du Sud. Il compte plus de dix années d’expérience 
+          en vérification et en fiscalité. Il a participé à la conception et à la mise en place de 
+          systèmes comptables et de systèmes de contrôle interne pour des sociétés internationales.<br/>
+          Sous la direction de la vice-présidente, finance et investissements, José gère aujourd’hui 
+          l’équipe de comptabilité où il est appelé à veiller au bon fonctionnement des contrôles 
+          internes et à la divulgation de l’information financière.</p></div>
+        <div class="col-md-2"></div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-3"><h3>Marie-Christine Barrette, LL.B., BAA</h3></div>
+        <div class="col-md-5"><p>JMarie-Christine occupe le poste de directrice, gestion et 
+          investissements. Lors de l’acquisition d’une entreprise, Marie-Christine  travaille 
+          de concert avec l’équipe de direction afin de développer des stratégies de croissance 
+          qui s’appuient sur les forces existantes de l’entreprise. Elle assure également le suivi 
+          des investissements effectués par Aliston.<br/>
+          SMarie-Christine possède dix-huit années d’expérience en droit des affaires et des sociétés, 
+          dont 8 ans dans le domaine du financement des sociétés à titre de gestionnaire au sein de 
+          l’Autorité des marchés financiers.</p></div>
+        <div class="col-md-2"></div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-3"><h3>Audrey Lanctôt, LL.B.</h3></div>
+        <div class="col-md-5"><p>Audrey agit à titre de responsable des affaires corporatives. 
+          En plus d’avoir pratiqué plusieurs années à titre de parajuriste au sein de deux importants 
+          cabinets d’avocats canadiens, elle est titulaire d’un baccalauréat en droit et est membre 
+          du Barreau du Québec.<br/>
+          Sous la supervision du directeur, affaires juridiques, Audrey collabore, entre autres, à la 
+          préparation de documents corporatifs et participe également aux vérifications diligentes dans 
+          le cadre des diverses transactions commerciales.</p></div>
+        <div class="col-md-2"></div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-3"><b>Denis Labrecque</b></div>
+        <div class="col-md-5"><p>Denis a été propriétaire d’un groupe diversifié d’entreprises de 
+          fabrication aux États-Unis et au Canada pendant plus de 20 ans avant de fonder NorCap, en 
+          1990. Depuis le début de NorCap, Denis a agi en tant que conseiller en fusions et acquisitions 
+          sur le marché des moyennes entreprises, ce qui lui a permis de réaliser quelques 40 transactions 
+          totalisant une valeur de plus de 2 milliards de dollars dans les domaines de la fabrication, la 
+          distribution, les compagnies de services et les produits de consommation.</p></div>
+        <div class="col-md-2"></div>
+      </div>
   	</div>
-    <div class="row">
-	  <div class="col-lg-4"></div>
-	  <div class="col-lg-8">Largeur 8</div>
 	</div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
