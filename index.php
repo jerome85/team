a73d708c7c026f2b53b644d824b94e5bbb1474c2
jerome85 <jerome.nourrain85@gmail.com>
2015-04-27 16:18:56 -0400

    16h18

--- index.php
+++ index.php
@@ -29,37 +29,47 @@
     </div>
     <div id="photo">
       <div class="row">
-        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe1.png"/></div>
-        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe2.png"/></div>
-        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe3.png"/></div>
-        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe4.png"/></div>
-        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe5.png"/></div>
-        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe6.png"/></div>
-        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe1.png"/></div>
-        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe2.png"/></div>
-        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe3.png"/></div>
-        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe4.png"/></div>
-        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe5.png"/></div>
-        <div class="col-xs-4 col-sm-3 col-lg-1"><img src="img/equipe6.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img id="equipe1" class="clear" src="img/equipe1.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img id="equipe2" class="opaque" src="img/equipe2.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img id="equipe3" class="opaque" src="img/equipe3.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img id="equipe4" class="opaque" src="img/equipe4.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img id="equipe5" class="opaque" src="img/equipe5.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img id="equipe6" class="opaque" src="img/equipe6.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img id="equipe7" class="opaque" src="img/equipe1.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img id="equipe8" class="opaque" src="img/equipe2.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img id="equipe9" class="opaque" src="img/equipe3.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img id="equipe10" class="opaque" src="img/equipe4.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img id="equipe11" class="opaque" src="img/equipe5.png"/></div>
+        <div class="col-xs-4 col-sm-3 col-lg-1"><img id="equipe12" class="opaque" src="img/equipe6.png"/></div>
       </div>
     </div>
 
     <div class="container">
-      <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-3"><h3>Stéphane Bégin, LL.B.</h3></div>
-        <div class="col-md-5"><p>Avocat de formation, Stéphane est diplômé en droit et membre du Barreau 
+      <div id="equipe1div" class="row div">
+        <div class="col-md-5">
+          <p>
+            <b>Stéphane Bégin, LL.B.</b><br/>
+            Président<br/>
+            <a href="mailto:sbegin@aliston.com">sbegin@aliston.com</a>
+          </p>
+        </div>
+        <div class="col-md-7"><p>Avocat de formation, Stéphane est diplômé en droit et membre du Barreau 
           du Québec. Après avoir exercé le droit en pratique privée, il a réorienté sa carrière vers 
           le milieu des affaires. Depuis 1995, avec son équipe, il a procédé à une série d’acquisitions 
           d’entreprises œuvrant dans différents secteurs d’activités. Fondateur d’Aliston, il y siège 
           au conseil d’administration et agit à titre de président. Enfin, il siège au conseil 
           d’administration de nombreuses sociétés dans lesquelles le groupe détient des participations.</p>
         </div>
-        <div class="col-md-2"></div>
       </div>
-      <div class="row">
+      <div id="equipe2div"class="row div">
         <div class="col-md-2"></div>
-        <div class="col-md-3"><h3>Olivier L.-Labonté, LL.B., MBA</h3></div>
+        <div class="col-md-3">
+          <p>
+            <b>Bertin Thériault, BAA, MBA</b><br/>
+            Vice-président, investissements<br/>
+            <a href="mailto:btheriault@aliston.com">btheriault@aliston.com</a>
+          </p>
+        </div>
         <div class="col-md-5"><p>Olivier agit à titre de directeur, affaires juridiques. 
           Avocat de formation et membre du Barreau du Québec, il est également titulaire 
           d’une maîtrise en administration des affaires (MBA). Avant de se joindre à notre 
@@ -70,9 +80,15 @@
           d’investissements.</p></div>
         <div class="col-md-2"></div>
       </div>
-      <div class="row">
+      <div id="equipe3div" class="row div">
         <div class="col-md-2"></div>
-        <div class="col-md-3"><h3>Caroline Martel, CPA.CA</h3></div>
+        <div class="col-md-3">
+          <p>
+            <b>Caroline Martel, CPA.CA</b><br/>
+            Vice-présidente, finance et investissements<br/>
+            <a href="mailto:cmartel@aliston.com">cmartel@aliston.com</a>
+          </p>
+        </div>
         <div class="col-md-5"><p>Comptable professionnelle agréée (CPA, CA), Caroline exerce sa 
           profession depuis 2001. Ayant d’abord travaillé au sein de grands cabinets, elle a ensuite 
           occupé le poste de Directrice des finances d’une société publique canadienne en Europe et en 
@@ -81,9 +97,15 @@
           en étant responsable de la gestion financière de l’entreprise.</p></div>
         <div class="col-md-2"></div>
       </div>
-      <div class="row">
+      <div id="equipe4div" class="row div">
         <div class="col-md-2"></div>
-        <div class="col-md-3"><h3>Jose Imbaquingo</h3></div>
+        <div class="col-md-3">
+          <p>
+            <b>Jose Imbaquingo</b><br/>
+            Contrôleur<br/>
+            <a href="mailto:jimbaquingo@aliston.com">jimbaquingo@aliston.com</a>
+          </p>
+        </div>
         <div class="col-md-5"><p>José agit à titre de contrôleur. Il a acquis son expertise au sein 
           de grands cabinets comptables en Amérique du Sud. Il compte plus de dix années d’expérience 
           en vérification et en fiscalité. Il a participé à la conception et à la mise en place de 
@@ -93,10 +115,16 @@
           internes et à la divulgation de l’information financière.</p></div>
         <div class="col-md-2"></div>
       </div>
-      <div class="row">
+      <div id="equipe5div" class="row div">
         <div class="col-md-2"></div>
-        <div class="col-md-3"><h3>Marie-Christine Barrette, LL.B., BAA</h3></div>
-        <div class="col-md-5"><p>JMarie-Christine occupe le poste de directrice, gestion et 
+        <div class="col-md-3">
+          <p>
+            <b>Marie-Christine Barrette, LL.B., BAA</b><br/>
+            Directrice, gestion et investissements<br/>
+            <a href="mailto:mcbarrette@aliston.com">mcbarrette@aliston.com</a>
+          </p>
+        </div>
+        <div class="col-md-5"><p>Marie-Christine occupe le poste de directrice, gestion et 
           investissements. Lors de l’acquisition d’une entreprise, Marie-Christine  travaille 
           de concert avec l’équipe de direction afin de développer des stratégies de croissance 
           qui s’appuient sur les forces existantes de l’entreprise. Elle assure également le suivi 
@@ -106,9 +134,15 @@
           l’Autorité des marchés financiers.</p></div>
         <div class="col-md-2"></div>
       </div>
-      <div class="row">
+      <div id="equipe6div" class="row div">
         <div class="col-md-2"></div>
-        <div class="col-md-3"><h3>Audrey Lanctôt, LL.B.</h3></div>
+        <div class="col-md-3">
+          <p>
+            <b>Audrey Lanctôt, LL.B.</b><br/>
+            Responsable des affaires corporatives<br/>
+            <a href="mailto:alanctot@aliston.com">alanctot@aliston.com</a>
+          </p>
+        </div>
         <div class="col-md-5"><p>Audrey agit à titre de responsable des affaires corporatives. 
           En plus d’avoir pratiqué plusieurs années à titre de parajuriste au sein de deux importants 
           cabinets d’avocats canadiens, elle est titulaire d’un baccalauréat en droit et est membre 
@@ -118,9 +152,35 @@
           le cadre des diverses transactions commerciales.</p></div>
         <div class="col-md-2"></div>
       </div>
-      <div class="row">
+      <div id="equipe7div" class="row div">
         <div class="col-md-2"></div>
-        <div class="col-md-3"><b>Denis Labrecque</b></div>
+        <div class="col-md-3">
+          <p>
+            <b>Audrey Teasdale, CPA, CMA</b><br/>
+            Analyste financière<br/>
+            <a href="mailto:ateasdale@aliston.com">ateasdale@aliston.com</a>
+          </p>
+        </div>
+        <div class="col-md-5"><p>Membre de l’Ordre des Comptables Professionnels Agréés du Québec 
+          (CPA, CMA), Audrey est titulaire d’un baccalauréat en commerce. Elle a débuté sa carrière 
+          au sein de cabinets comptables d’envergure internationale.  Au cours des années, Audrey a 
+          acquis de l’expérience en tant qu’analyste financière dans différentes industries, notamment 
+          dans les secteurs manufacturier et minier ainsi que dans la restauration. De plus, elle siège 
+          à titre d’administratrice sur le conseil d’administration de la Fondation Centre 
+          François-Michelle.</p></div>
+        <div class="col-md-2"></div>
+      </div>
+      <div id="equipe8div" class="row div">
+        <div class="col-md-2"></div>
+        <div class="col-md-3">
+          <p>
+            <b>Denis Labrecque</b><br/>
+            Président NorCap Canada Ltée,<br/>
+            Conseiller spécial du président,<br/>
+            Membre du réseau «Association for Corporate Growth»<br/>
+            <a href="mailto:cmartel@aliston.com">denis.labrecque@norcap.ca</a>
+          </p> 
+        </div>
         <div class="col-md-5"><p>Denis a été propriétaire d’un groupe diversifié d’entreprises de 
           fabrication aux États-Unis et au Canada pendant plus de 20 ans avant de fonder NorCap, en 
           1990. Depuis le début de NorCap, Denis a agi en tant que conseiller en fusions et acquisitions 
@@ -133,5 +193,6 @@
 	</div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
   </body>
 </html>
